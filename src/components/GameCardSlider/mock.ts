31393db99d97b8e8df6cff093d4596472f3f24d5
Anderson <andersonfonseca1999@hotmail.com>
2021-06-20 20:48:06 -0300

    Fix folder name

--- src/components/GameCardSlider/mock.ts
+++ src/components/GameCardSlider/mock.ts
@@ -1,9 +1,4 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-
-import { GameCardProps } from 'components/GameCard'
-import GameCardSlider from '.'
-
-const items = [
+export default [
   {
     title: 'Population Zero',
     developer: 'Rockstar Games',
@@ -47,21 +42,3 @@ const items = [
     promotionalPrice: 'R$ 215,00'
   }
 ]
-
-export default {
-  title: 'GameCardSlider',
-  component: GameCardSlider,
-  args: { items },
-  parameters: {
-    layout: 'fullscreen',
-    backgrounds: {
-      default: 'won-dark'
-    }
-  }
-} as Meta
-
-export const Default: Story<GameCardProps[]> = (args) => (
-  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
-    <GameCardSlider items={args} {...args} />
-  </div>
-)
