28fe4203d08b18ad5070f357fbc81afa58b27f92
Anderson <andersonfonseca1999@hotmail.com>
2021-06-23 16:14:14 -0300

    Add TextField component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,43 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import TextField, { TextFieldProps } from '.'
+
+import { Email } from '@styled-icons/material-outlined'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    icon: <Email />,
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'changed' },
+    icon: { type: '' },
+    iconPosition: {
+      type: 'radio',
+      options: ['left', 'right']
+    },
+    disabled: { type: 'boolean' },
+    error: { type: 'string' }
+  }
+} as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...something is wrong'
+}
