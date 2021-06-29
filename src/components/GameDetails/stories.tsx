09869af5a8f7457b26874f0a067c06b219845270
Anderson <andersonfonseca1999@hotmail.com>
2021-06-28 22:23:55 -0300

    Add GameDetails component

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,41 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
+
+export default {
+  title: 'Game/GameDetails',
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame,
+  argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative']
+      }
+    },
+    rating: {
+      type: 'radio',
+      options: ['BR0', 'BR10', 'BR12', 'BR14', 'BR16', 'BR18']
+    }
+  }
+} as Meta
+
+export const Default: Story<GameDetailsProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameDetails {...args} />
+  </div>
+)
