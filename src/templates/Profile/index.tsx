2a4863cd4efba3f623b240d1d87a401885c5bae7
Anderson <andersonfonseca1999@hotmail.com>
2021-07-07 14:51:45 -0300

    Add Profile template

---
+++ src/templates/Profile/index.tsx
@@ -0,0 +1,33 @@
+import * as S from './styles'
+
+import { useRouter } from 'next/router'
+
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+import ProfileMenu from 'components/ProfileMenu'
+import Base from 'templates/Base'
+
+export type ProfileTemplateProps = {
+  children: React.ReactNode
+}
+
+const Profile = ({ children }: ProfileTemplateProps) => {
+  const { asPath } = useRouter()
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My profile
+        </Heading>
+
+        <S.Main>
+          <ProfileMenu activeLink={asPath} />
+          <S.Content>{children}</S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  )
+}
+
+export default Profile
