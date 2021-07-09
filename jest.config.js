0716efbe235d4c00443c717cdaee2cd43c262fb1
Anderson <andersonfonseca1999@hotmail.com>
2021-07-09 19:02:16 -0300

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,10 @@ module.exports = {
     '!src/**/stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
+    '!src/utils/apollo.ts',
+    '!src/utils/apolloCache.ts',
+    '!src/types/**/*.d.ts',
+    '!src/graphql/**/*.ts',
     '!src/**/mock.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
