24f1a67d091a1db21d3e1aef842f97b31b53f72d
Anderson <andersonfonseca1999@hotmail.com>
2021-07-13 18:43:04 -0300

    Adjust CartDropdown to remove props drilling

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -3,17 +3,11 @@ import * as S from './styles'
 import CartIcon from 'components/CartIcon'
 import CartList from 'components/CartList'
 import Dropdown from 'components/Dropdown'
-import { GameItemProps } from 'components/GameItem'
 
-export type CartDropdownProps = {
-  items?: GameItemProps[]
-  total?: string
-}
-
-const CartDropdown = ({ items, total }: CartDropdownProps) => (
+const CartDropdown = () => (
   <S.Wrapper>
-    <Dropdown title={<CartIcon quantity={items?.length} />}>
-      <CartList items={items} total={total} hasButton />
+    <Dropdown title={<CartIcon />}>
+      <CartList hasButton />
     </Dropdown>
   </S.Wrapper>
 )
