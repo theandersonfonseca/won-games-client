a0688109c178f2bc20ebb4166099c3a2da74433b
Anderson <andersonfonseca1999@hotmail.com>
2021-07-13 13:34:35 -0300

    Fix revalidate and using no-cache from Apollo

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -47,7 +47,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     QueryGameBySlugVariables
   >({
     query: QUERY_GAME_BY_SLUG,
-    variables: { slug: `${params?.slug}` }
+    variables: { slug: `${params?.slug}` },
+    fetchPolicy: 'no-cache'
   })
 
   if (!data.games.length) {
@@ -67,8 +68,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   >({ query: QUERY_UPCOMING, variables: { date: TODAY } })
 
   return {
+    revalidate: 60,
     props: {
-      revalidate: 60,
       cover: `http://localhost:1337${game.cover?.src}`,
       gameInfo: {
         title: game.name,
