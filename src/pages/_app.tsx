db69bed53548fe6c6e041430922e173216516b2a
Anderson <andersonfonseca1999@hotmail.com>
2021-07-13 17:40:08 -0300

    Add useCart hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 
 import { ThemeProvider } from 'styled-components'
 import { ApolloProvider } from '@apollo/client'
+import { CartProvider } from 'hooks/use-cart'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
@@ -15,14 +16,19 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#06092B" />
-          <meta name="description" content="The best Game Store in the world" />
-        </Head>
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#06092B" />
+            <meta
+              name="description"
+              content="The best Game Store in the world"
+            />
+          </Head>
+        </CartProvider>
         <GlobalStyles />
         <Component {...pageProps} />
       </ThemeProvider>
