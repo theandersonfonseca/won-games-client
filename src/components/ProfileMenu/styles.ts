c8b58ad88671617ccaf53efc6cebf9b1ab3d1371
Anderson <andersonfonseca1999@hotmail.com>
2021-07-16 16:21:25 -0300

    Add signOut to Dropdown and ProfileMenu

--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -34,6 +34,7 @@ type LinkProps = {
 
 export const Link = styled.a<LinkProps>`
   ${({ theme, isActive }) => css`
+    cursor: pointer;
     background: ${theme.colors.white};
     color: ${theme.colors.black};
     text-decoration: none;
