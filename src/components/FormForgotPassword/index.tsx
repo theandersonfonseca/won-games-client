8f506d154d4de161d9df0307742e51c8e53d42df
Anderson <andersonfonseca1999@hotmail.com>
2021-07-18 13:52:10 -0300

    Add ForgotPassword Page and FormForgotPassword

---
+++ src/components/FormForgotPassword/index.tsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
+
+import { Email, ErrorOutline } from '@styled-icons/material-outlined'
+
+import { FormWrapper, FormLoading, FormError } from 'components/Form'
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+
+import { FieldErrors } from 'utils/validations'
+
+const FormForgotPassword = () => {
+  const [formError, setFormError] = useState('')
+  const [fieldError, setFieldError] = useState<FieldErrors>({})
+  const [values, setValues] = useState({ email: '' })
+  const [loading, setLoading] = useState(false)
+  const routes = useRouter()
+  const { push, query } = routes
+
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setLoading(true)
+
+    const errors = {} // validate after
+
+    if (Object.keys(errors).length) {
+      setFieldError(errors)
+      setLoading(false)
+      return
+    }
+
+    setFieldError({})
+
+    // sign in
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
+    })
+
+    if (result?.url) {
+      return push(result?.url)
+    }
+
+    setLoading(false)
+
+    // jogar o erro
+    setFormError('username or password is invalid')
+  }
+
+  return (
+    <FormWrapper>
+      {!!formError && (
+        <FormError>
+          <ErrorOutline /> {formError}
+        </FormError>
+      )}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          error={fieldError?.email}
+          onInputChange={(v) => handleInput('email', v)}
+          icon={<Email />}
+        />
+
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Send email</span>}
+        </Button>
+      </form>
+    </FormWrapper>
+  )
+}
+
+export default FormForgotPassword
