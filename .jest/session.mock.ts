0d3a5678b52b7e9560b0538b06445f059caa19b3
Anderson <andersonfonseca1999@hotmail.com>
2021-07-20 17:24:41 -0300

    Fix module bug

--- .jest/session.mock.ts
+++ .jest/session.mock.ts
@@ -2,3 +2,5 @@
 const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
 const session = { jwt: '123', user: { email: 'lorem@ipsum.com' } }
 useSession.mockImplementation(() => [session])
+
+export {}
