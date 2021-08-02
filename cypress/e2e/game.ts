d0d060fb33be8a3e86e169e43da4f0fc799c53b2
Anderson <andersonfonseca1999@hotmail.com>
2021-08-02 17:23:32 -0300

    Start e2e tests to Game page

---
+++ cypress/e2e/game.ts
@@ -0,0 +1,17 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Game Page', () => {
+  it('should render game page sections', () => {
+    cy.visit('/game/no-mans-sky')
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('heading', { name: /no man's sky/i }).should('exist')
+      cy.findByText(/^Inspired by the adventure and imagination that we love from classic science-fiction/i).should('exist')
+      cy.findByText('$64.99').should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
+
+    // gallery
+    cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0)
+  });
+});
