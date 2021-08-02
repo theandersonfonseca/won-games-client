cb81a6abe9798e27718eea8066208ab1021d6e35
Anderson <andersonfonseca1999@hotmail.com>
2021-08-02 16:39:31 -0300

    Get GameCard inside showcase

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -53,5 +53,7 @@ Cypress.Commands.add('shouldRenderShowcase', ({ name, highlight = false }) => {
         cy.findByRole('link').should('have.attr', 'href')
       })
     }
+
+    cy.get(`[data-cy="game-card"]`).should('have.length.gt', 0)
   })
 })
