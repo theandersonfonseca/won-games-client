1200a5c871f2c3c12cdf58a3935292d8436ec9d5
Anderson <andersonfonseca1999@hotmail.com>
2021-08-04 16:31:41 -0300

    Remove stories from PaymentForm

--- src/components/PaymentForm/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import PaymentForm from '.'
-
-export default {
-  title: 'PaymentForm',
-  component: PaymentForm
-} as Meta
-
-export const Default: Story = () => <PaymentForm />
